1c4f3068da8cd38339920ac121970d3644f53390
Ali WaseeM <148793133+AliWaseem786@users.noreply.github.com>
2024-03-23 01:35:11 -0700

    number Game

---
+++ app.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let numberGame = await inquirer.prompt([
+    { message: "Guess a Number", type: "number", name: "GuessedNumber" }
+]);
+console.log(numberGame);
+if (numberGame.GuessedNumber === randomNumber) {
+    console.log("Congratulation! You Won");
+}
+else {
+    console.log("Please Try Again");
+}
